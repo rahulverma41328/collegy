c6a46c8c495f6dc81ddb9f8a68dabd70c471a40f
rahul verma <rahulverma41328@gmail.com>
2024-05-22 23:52:44 +0530

    rahul verma

--- app/src/main/java/com/example/collegeconnected/activities/Dashboard.kt
+++ app/src/main/java/com/example/collegeconnected/activities/Dashboard.kt
@@ -1,11 +1,13 @@
 package com.example.collegeconnected.activities
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.collegeconnected.R
 import com.example.collegeconnected.databinding.ActivityDashboardBinding
@@ -18,7 +20,7 @@ class Dashboard : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding =  ActivityDashboardBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_dashboard)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
